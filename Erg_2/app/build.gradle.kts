0e254946c6aedab00bc0fdd1820d1383595c15ac
OGecgo <oian72116@gmail.com>
2026-01-18 15:54:04 +0200

    start erg2

---
+++ Erg_2/app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+
+     // Dependency google firebase
+     id("com.google.gms.google-services")
+     id("com.android.application")
+
+//     alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.unipifirechat"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.unipifirechat"
+        minSdk = 33
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // Dependency google firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+    // add dep
+
+}
